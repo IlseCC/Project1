8f42dcaccda863845468afec95298c767ecd951f
ILSE <icontreras@oasishoteles.com>
2018-10-18 16:52:14 -0500

    Agregar archivos de proyecto.

---
+++ Project1/Project1/urls.py
@@ -0,0 +1,21 @@
+"""
+Definition of urls for Project1.
+"""
+
+from django.conf.urls import include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', Project1.views.home, name='home'),
+    # url(r'^Project1/', include('Project1.Project1.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+]
